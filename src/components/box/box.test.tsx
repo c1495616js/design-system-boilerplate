a3adbe6f4f754bbeb92f48d98af5b83ff73f61b3
Jerry <jerryappleid761208@gmail.com>
2021-08-08 00:25:59 -0700

    add test

--- src/components/box/box.test.tsx
+++ src/components/box/box.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import {Box} from '.'
+import { Box } from '.';
 
 describe('Box', () => {
   it('renders primary variant', () => {
-    render(<Box>This is box</Box>)
+    render(<Box>This is box</Box>);
     expect(screen.getByText(/this is box/i)).toBeDefined();
   });
 });
